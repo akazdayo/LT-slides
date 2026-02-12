0c25bcbc54285b48a07d9203575f0840bcc14be0
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-02-13 01:41:33 +0900

    format

--- flake.nix
+++ flake.nix
@@ -2,17 +2,21 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-	  bun
-	  nodejs_24
-        ];
-      };
-    }
-  );
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bun
+            nodejs_24
+          ];
+        };
+      }
+    );
 }
